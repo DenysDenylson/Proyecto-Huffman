7a2657e90693ad6e382eca0d32b744a76d23919e
DenysDenylson <DenysDenylson@gmail.com>
2015-11-12 12:04:13 -0400

    saludando a alex desde local 2

--- ProyectoHuffman/ProyectoHuffman/main.cpp
+++ ProyectoHuffman/ProyectoHuffman/main.cpp
@@ -6,6 +6,7 @@ int main(){
 	cout << "Hello world..!!!!!" << endl;
 	cout << "Saludando a alex" << endl;
 	cout << "Saludando desde local a alex" << endl;
+	cout << "Saludando desde local_huffman_2 a alex" << endl;
 
 
 	system("pause");
